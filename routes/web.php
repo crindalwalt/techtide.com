db9f031824291c73a45dece6a0b49bb8d5d8dc57
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-08-07 13:30:47 +0500

    breeze scaffolding done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/",[HomeController::class,'index'])->name("home");
 Route::get("/blog-detail",[HomeController::class,"show"])->name("blog.detail");
+
+
+
+Route::get("/admin",function () {
+    return "ap admin waly page per ponch gye hn";
+})->name("admin");
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
